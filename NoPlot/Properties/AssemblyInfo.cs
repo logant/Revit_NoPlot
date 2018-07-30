8145c17fdde434100eabac4c46b946e055ec550c
logant <timothy.b.logan@gmail.com>
2018-07-30 14:35:23 -0500

    Adding Elk to assembly's Company name attribute.

--- NoPlot/Properties/AssemblyInfo.cs
+++ NoPlot/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NoPlot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Elk")]
 [assembly: AssemblyProduct("NoPlot")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
